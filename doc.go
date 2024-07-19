e36a26ef5f3718e4880cff66cd5df7ac487f7873
MarvinJWendt <git@marvinjwendt.com>
2024-07-19 23:59:37 +0200

    docs: better example

--- doc.go
+++ doc.go
@@ -2,26 +2,5 @@
 Package splitslog provides a handler that splits log records to different handlers based on their level.
 
 The most common use case is to split logs to stdout and stderr based on their level.
-
-	func main() {
-		splitter := splitslog.Splitter{
-			// Debug and info messages are printed to stdout.
-			slog.LevelDebug: slog.NewJSONHandler(os.Stdout, nil),
-			slog.LevelInfo:  slog.NewJSONHandler(os.Stdout, nil),
-			// Warn and error messages are printed to stderr.
-			slog.LevelWarn:  slog.NewJSONHandler(os.Stderr, nil),
-			slog.LevelError: slog.NewJSONHandler(os.Stderr, nil),
-		}
-
-		handler := splitslog.NewSplitHandler(splitter)
-		logger := slog.New(handler)
-
-		logger.Info("info message prints to stdout")
-		logger.Error("error message prints to stderr")
-
-		// Output:
-		// stdout: {"time":"2023-09-07T16:56:22.563817+02:00","level":"INFO","msg":"info message prints to stdout"}
-		// stderr: {"time":"2023-09-07T16:56:22.564103+02:00","level":"ERROR","msg":"error message prints to stderr"}
-	}
 */
 package splitslog
--- examples_test.go
+++ examples_test.go
@@ -7,11 +7,33 @@ import (
 	"atomicgo.dev/splitslog"
 )
 
+func Example_demo := splitslog.Splitter{
+		// Debug and info messages are printed to stdout.
+		slog.LevelDebug: slog.NewJSONHandler(os.Stdout, nil),
+		slog.LevelInfo:  slog.NewJSONHandler(os.Stdout, nil),
+
+		// Warn and error messages are printed to stderr.
+		slog.LevelWarn:  slog.NewJSONHandler(os.Stderr, nil),
+		slog.LevelError: slog.NewJSONHandler(os.Stderr, nil),
+	}
+
+	handler := splitslog.NewSplitHandler(splitter)
+	logger := slog.New(handler)
+
+	logger.Info("info message prints to stdout")
+	logger.Error("error message prints to stderr")
+
+	// stdout: {"time":"2023-09-07T16:56:22.563817+02:00","level":"INFO","msg":"info message prints to stdout"}
+	// stderr: {"time":"2023-09-07T16:56:22.564103+02:00","level":"ERROR","msg":"error message prints to stderr"}
+}
+
+
 func ExampleNewSplitHandler() {
 	splitter := splitslog.Splitter{
 		// Debug and info messages are printed to stdout.
 		slog.LevelDebug: slog.NewJSONHandler(os.Stdout, nil),
 		slog.LevelInfo:  slog.NewJSONHandler(os.Stdout, nil),
+
 		// Warn and error messages are printed to stderr.
 		slog.LevelWarn:  slog.NewJSONHandler(os.Stderr, nil),
 		slog.LevelError: slog.NewJSONHandler(os.Stderr, nil),
